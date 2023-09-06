f47af525f85bca6d4f953f07b2da734e6ebe8b66
Islam Ibrahim <islamibrahim.email@gmail.com>
2023-09-06 21:37:03 +0300

    fixes

--- src/components/coming-soon/coming-soon.tsx
+++ src/components/coming-soon/coming-soon.tsx
@@ -18,7 +18,7 @@ const ComingSoon: React.FC = ({ className }: ComingSoonProps) => {
             <div className={styles.container}>
                 <div className={styles.leftSide}>
                     <div className={styles.sideNavDiv}>
-                        <SideNavBar selectedIndex={0} />
+                        <SideNavBar selectedIndex={null} />
                     </div>
                     <div className={styles.suggestedActivityDiv}>
                         <SuggestedActivity showActivity={true} showSuggestedContent={true} />
