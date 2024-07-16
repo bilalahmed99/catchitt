2373b3ee8e7a07f1795b40e9eef06d95804bc4d7
Ehsanulhaq <ehsanulhaq@ogoul.com>
2024-07-16 10:25:57 +0500

    no message

--- src/components/stories/icons/pause.tsx
+++ src/components/stories/icons/pause.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Pause = () => {
   return (
     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8 5V19M16 5V19" stroke="#5448B2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8 5V19M16 5V19" stroke="rgb(255, 59, 92)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   )
 }
