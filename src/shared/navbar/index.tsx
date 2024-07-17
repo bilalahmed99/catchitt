a6c0874d47cc279f460a5e36fd21b12577f47ac3
Ehsanulhaq <ehsanulhaq@ogoul.com>
2024-07-17 10:41:19 +0500

    no message

--- src/shared/navbar/index.tsx
+++ src/shared/navbar/index.tsx
@@ -65,12 +65,18 @@ function Navbar() {
                     />
                     {isLoggedIn ? (
                         <div className={style.profile}>
-                            <img
+                            {/* <img
                                 style={{ width: 36, height: 36, cursor: 'pointer' }}
                                 src={createIcon}
                                 alt=""
                                 onClick={() => navigate('/upload')}
-                            />
+                            /> */}
+                            {/* <a href="/tiktokstudio/upload?from=upload" target="_self" className="e18d3d942 css-2gvzau-ALink-StyledLink er1vbsz1" aria-label="Upload a video"> */}
+                                <div className={style.DivUpload} style={{ width: 36, height: 36, cursor: 'pointer' }} onClick={() => navigate('/upload')}>
+                                    <svg className="" width="1em" data-e2e="" height="1em" viewBox="0 0 16 16" fill="rgb(255, 59, 92)" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 2.5C7.58579 2.5 7.25 2.83579 7.25 3.25V7.25H3.25C2.83579 7.25 2.5 7.58579 2.5 8C2.5 8.41421 2.83579 8.75 3.25 8.75H7.25V12.75C7.25 13.1642 7.58579 13.5 8 13.5C8.41421 13.5 8.75 13.1642 8.75 12.75V8.75H12.75C13.1642 8.75 13.5 8.41421 13.5 8C13.5 7.58579 13.1642 7.25 12.75 7.25H8.75V3.25C8.75 2.83579 8.41421 2.5 8 2.5Z"></path>
+                                    </svg>
+                                    <span style={{color:'rgb(255, 59, 92)'}}>Upload </span>
+                                </div>
                             <div className={style.user}>
                                 <div
                                     style={{ position: 'relative', width: '100%', height: '100%' }}
@@ -128,12 +134,13 @@ function Navbar() {
                 </div>
             ) : (
                 <div style={{ position: 'relative' }}>
-                    <img
+                    {/* <img
                         style={{ width: '40px', height: '40px', borderRadius: '50%' }}
                         src={profile?.avatar || defaultAvatar}
                         onClick={() => navigate('/upload')}
                         alt=""
-                    />
+                    /> */}
+                    {/* <svg className="css-qeydvm-StyledPlusIcon e18d3d945" width="1em" data-e2e="" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 2.5C7.58579 2.5 7.25 2.83579 7.25 3.25V7.25H3.25C2.83579 7.25 2.5 7.58579 2.5 8C2.5 8.41421 2.83579 8.75 3.25 8.75H7.25V12.75C7.25 13.1642 7.58579 13.5 8 13.5C8.41421 13.5 8.75 13.1642 8.75 12.75V8.75H12.75C13.1642 8.75 13.5 8.41421 13.5 8C13.5 7.58579 13.1642 7.25 12.75 7.25H8.75V3.25C8.75 2.83579 8.41421 2.5 8 2.5Z"></path></svg> */}
                     <NavbarMunu
                         onViewProfile={() => navigate(`/profile`)}
                         Onlogout={() => logoutAccount()}
