5234013a3bc0926276bf912b6b52b62def60c78b
Muhammad Asif <masif@ogoul.com>
2025-02-25 10:56:53 +0500

    chat design updates

--- src/components/user-chat/components/Actions.tsx
+++ src/components/user-chat/components/Actions.tsx
@@ -101,7 +101,7 @@ function Actions(props: any) {
                             <div className={style.msg}>
                                 {item.emojis && item.dropdown && (
                                     <div
-                                        className={`flex ${style.dropd} ${isDarkTheme ? 'bg-[#353434]' : 'bg-gray-300'} shadow-md ${item.receiverId == loggedInUserId?'-right-20':'right-2'} top-6 z-50`}
+                                        className={`flex ${style.dropd} ${isDarkTheme ? 'bg-[#353434]' : 'bg-white'} ${item.receiverId == loggedInUserId?'-right-20':'right-2'} top-6 z-50`}
                                     >
                                         <div
                                             style={{
@@ -113,12 +113,13 @@ function Actions(props: any) {
                                                 valuesH2(item, 'stared', !item.stared);
                                             }}
                                         >
-                                            {/* <img src={!item.stared ? starMsg : unStarMsg} alt="" /> */}
+                                            <svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.9998 16.75L4.82784 19.995L6.00684 13.122L1.00684 8.25495L7.90684 7.25495L10.9928 1.00195L14.0788 7.25495L20.9788 8.25495L15.9788 13.122L17.1578 19.995L10.9998 16.75Z" stroke="#B6B6B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
                                             <p color="#222" className={style.dropdText}>
                                                 {item.stared ? 'UnStar' : 'Star'}
                                             </p>
                                         </div>
-                                        <hr className={style.hr} />
 
                                         <div
                                             style={{
@@ -131,19 +132,21 @@ function Actions(props: any) {
                                                 openDeleteModal(item);
                                             }}
                                         >
-                                            {/* <img src={deleteMsg} alt="" /> */}
-                                            <p
-                                                // style={{
-                                                //     color: '#DE0C0C',
-                                                // }}
-                                                className={style.dropdText}
-                                            >
-                                                Delete
-                                            </p>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <path d="M4 7H20" stroke="#FF2C55" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M10 11V17" stroke="#FF2C55" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M14 11V17" stroke="#FF2C55" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7" stroke="#FF2C55" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7" stroke="#FF2C55" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                </svg>
+                                                <p
+                                                     
+                                                    className={style.dropdText} style={{ color: '#FF2C55'}} >
+                                                    Delete
+                                                </p>
                                         </div>
                                         {/* {item.isrecevied && ( */}
                                             <>
-                                                <hr className={style.hr} />
                                                 <div
                                                     onClick={(e) => {
                                                         e.stopPropagation();
@@ -155,14 +158,16 @@ function Actions(props: any) {
                                                         width: '100%',
                                                     }}
                                                 >
-                                                    {/* <img src={copyMsg} alt="" /> */}
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                        <path d="M7 9.667C7 8.95967 7.28099 8.28131 7.78115 7.78115C8.28131 7.28099 8.95967 7 9.667 7H18.333C18.6832 7 19.03 7.06898 19.3536 7.20301C19.6772 7.33704 19.9712 7.53349 20.2189 7.78115C20.4665 8.0288 20.663 8.32281 20.797 8.64638C20.931 8.96996 21 9.31676 21 9.667V18.333C21 18.6832 20.931 19.03 20.797 19.3536C20.663 19.6772 20.4665 19.9712 20.2189 20.2189C19.9712 20.4665 19.6772 20.663 19.3536 20.797C19.03 20.931 18.6832 21 18.333 21H9.667C9.31676 21 8.96996 20.931 8.64638 20.797C8.32281 20.663 8.0288 20.4665 7.78115 20.2189C7.53349 19.9712 7.33704 19.6772 7.20301 19.3536C7.06898 19.03 7 18.6832 7 18.333V9.667Z" stroke="#B6B6B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        <path d="M4.012 16.737C3.70534 16.5622 3.45027 16.3095 3.27258 16.0045C3.09488 15.6995 3.00085 15.353 3 15V5C3 3.9 3.9 3 5 3H15C15.75 3 16.158 3.385 16.5 4" stroke="#B6B6B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
                                                     <p color="#222" className={style.dropdText}>
                                                         Copy
                                                     </p>
                                                 </div>
                                             </>
                                         {/* )} */}
-                                        <hr className={style.hr} />
                                         <div
                                             style={{
                                                 cursor: 'pointer',
@@ -173,7 +178,9 @@ function Actions(props: any) {
                                                 forwardH(item);
                                             }}
                                         >
-                                            {/* <img src={deleteMsg} alt="" /> */}
+                                            <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M11.0002 1V5C4.42519 6.028 1.98019 11.788 1.00019 17C0.963188 17.206 6.38419 11.038 11.0002 11V15L19.0002 8L11.0002 1Z" stroke="#B6B6B6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
                                             <p
                                                 // style={{
                                                 //     color: '#DE0C0C',
